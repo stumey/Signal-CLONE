b969fc76593b24e1239f3f842e6f8296e6d971a8
Stefan Tumey <stef@Stefans-Air.lan>
2022-12-18 09:04:13 -0500

    Building out login screen, still a wip

--- App.js
+++ App.js
@@ -2,14 +2,23 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: {backgroundColor: "#2C6BED"},
+  headerTitleStyle: {color: "white"},
+  headerTitleColor: "white",
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-          <Text>Let's Build Signal!</Text>
-          <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
